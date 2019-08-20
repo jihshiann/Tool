22c59c9044467dd5f89fa8ea83f06af7288df18a
蔡季憲 <jixiantsai@lionmail.com>
2019-08-20 14:24:54 +0800

    Add two tiny tools

---
+++ AddBinary/AddBinary/AddBinary/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddBinary
+{
+    class Program
+    {
+        public static ulong sumInt = 0;
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Plz enter the first binary digit(limited in 20 bits)");
+                string inputA = Console.ReadLine();
+                Console.WriteLine("Plz enter the second binary digit(limited in 20 bits)");
+                string inputB = Console.ReadLine();
+                Console.WriteLine(string.Format("The sum in binary type is {0}, and decimal is {1} ", solution.AddBinary(inputA, inputB), sumInt));
+                Console.WriteLine(@"
+                Type Q to quit");
+                switch (Console.ReadLine().ToUpper())
+                {
+                    case "Q":
+                        repeat = false;
+                        break;
+
+                    default:
+                        repeat = true;
+                        Console.WriteLine("Add again");
+                        break;
+                }
+            }
+        }
+
+        public class Solution
+        {
+            public string AddBinary(string a, string b)
+            {
+                ulong aBinary = ulong.Parse(a);
+                ulong bBinary = ulong.Parse(b);
+                ulong aInt = 0;
+                ulong bInt = 0;
+                ulong countA = 0;
+                ulong countB = 0;
+                while (aBinary > 0)
+                {
+                    ulong times = 1;
+                    for (ulong i = 0; i < countA; i++)
+                    {
+                        times = 2 * times;
+                    }
+                    aInt = aInt + aBinary % 10 * times;
+                    aBinary /= 10;
+                    countA++;
+                }
+
+                while (bBinary > 0)
+                {
+                    ulong times = 1;
+                    for (ulong i = 0; i < countB; i++)
+                    {
+                        times = 2 * times;
+                    }
+                    bInt = bInt + bBinary % 10 * times;
+                    bBinary /= 10;
+                    countB++;
+                }
+                
+                sumInt = aInt + bInt;
+                ulong sumBinary = 0;
+                ulong countSum = 0;
+                ulong sumIntStored = sumInt;
+                while (sumIntStored > 0)
+                {
+                    ulong times = 1;
+                    for (ulong i = 0; i < countSum; i++)
+                    {
+                        times = 10 * times;
+                    }
+                    sumBinary = sumBinary + sumIntStored % 2 * times;
+                    sumIntStored /= 2;
+                    countSum++;
+                }
+
+
+                return sumBinary.ToString();
+            }
+        }
+    }
+}
---
+++ BinaryToDecimal_or_Reverse/BinaryToDecimal_or_Reverse/BinaryToDecimal_or_Reverse/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryToDecimal_or_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            bool repeat = true;
+            bool invalid = true;
+
+            while (repeat)
+            {
+                Console.WriteLine(@"
+Type 1 for make a binary to decimal
+Type 2 for make a decimal to binary");
+                while (invalid)
+                {
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            invalid = false;
+                            Console.WriteLine("Plz enter a binary string which is less than 20 characters");
+                            Console.WriteLine(solution.BinaryToDecimal(Console.ReadLine()));
+                            break;
+
+                        case "2":
+                            invalid = false;
+                            Console.WriteLine("Plz enter a decimal string which is less than 1048575");
+                            Console.WriteLine(solution.DecimalToBinary(Console.ReadLine()));
+                            break;
+
+                        default:
+                            invalid = true;
+                            Console.WriteLine("Just 1 or 2. Thx");
+                            break;
+                    }
+                }
+                Console.WriteLine(@"
+                Type Q to quit");
+                switch (Console.ReadLine().ToUpper())
+                {
+                    case "Q":
+                        repeat = false;
+                        break;
+
+                    default:
+                        repeat = true;
+                        invalid = true;
+                        Console.WriteLine("convert again");
+                        break;
+                }
+
+            }
+        }
+
+        public class Solution
+        {
+            public string BinaryToDecimal(string a)
+            {
+                ulong aBinary = ulong.Parse(a);
+                ulong aInt = 0;
+                ulong countA = 0;
+                while (aBinary > 0)
+                {
+                    ulong times = 1;
+                    for (ulong i = 0; i < countA; i++)
+                    {
+                        times = 2 * times;
+                    }
+                    aInt = aInt + aBinary % 10 * times;
+                    aBinary /= 10;
+                    countA++;
+                }
+                return aInt.ToString();
+            }
+
+            public string DecimalToBinary(string b)
+            {
+                ulong bInt = ulong.Parse(b);
+                ulong bBinary = 0;
+                ulong countB = 0;
+
+                while (bInt > 0)
+                {
+                    ulong times = 1;
+                    for (ulong i = 0; i < countB; i++)
+                    {
+                        times = 10 * times;
+                    }
+                    bBinary = bBinary + bInt % 2 * times;
+                    bInt /= 2;
+                    countB++;
+                }
+
+                return bBinary.ToString();
+            }
+        }
+    }
+}
